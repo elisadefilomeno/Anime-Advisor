47e1559dbe88d74a8683f3eacf6942b628062f67
SecondulfoValerio <rauros07@gmail.com>
2022-01-22 18:20:20 +0100

    Aggiunti metodi GET/SET classe Anime e corretti alcuni errori

--- src/main/java/it/unipi/large_scale/anime_advisor/entity/Anime.java
+++ src/main/java/it/unipi/large_scale/anime_advisor/entity/Anime.java
@@ -16,7 +16,47 @@ public final class Anime {
     private int scored; //total score obtained
     private int scoredby; //number of users that voted
     private int members; //number of followers
-
+    //SET METHODS
+    public void setAnime_id(String anime_id){
+        this.anime_id=anime_id;
+    }
+    public void setAnime_name(String anime_name){
+        this.anime_name=anime_name;
+    }
+    public void setPremiered(String premiered){
+        this.premiered=premiered;
+    }
+    public void setGenre(String[] genres){
+        this.genre=genres;
+    }
+    public void setType(String type){
+        this.type=type;
+    }
+    public void setEpisodes(int episodes){
+        this.episodes=episodes;
+    }
+    public void setProducer(String[] producer){
+        this.producer=producer;
+    }
+    public void setLicensor(String[] licensor){
+        this.licensor= licensor;
+    }
+    public void setStudio(String[] studio){
+        this.studio=studio;
+    }
+    public void setSource(String source){
+        this.source=source;
+    }
+    public void setScored(int scored){
+        this.scored=scored;
+    }
+    public void setScoredby(int scoredby){
+        this.scoredby=scoredby;
+    }
+    public void setMembers(int members){
+        this.members=members;
+    }
+    //GET METHODS
     public String getAnime_id(){
         return this.anime_id;
     }
@@ -57,40 +97,42 @@ public final class Anime {
         return this.members;
     }
     public String toString(){
-        String string;
-        string=(
+        StringBuilder string= new StringBuilder();
+        string.append(
             "Anime_id: " + this.anime_id + "\n" +
             "Name: " +this.anime_name +  "\n" +
             "Premiered: " +this.premiered + "\n" +
             "Genre: "
         );
         for(String i:this.genre){
-          string.concat(i +" ");
+          string.append(i +" ");
         }
-        string.concat("\n");
-      string.concat("" +
+        string.append("\n");
+      String str=("" +
                 "Type: "+this.type+"\n"+
                 "Episodes: "+this.episodes+"\n"
         );
+      string.append(str);
         for(String i:this.producer){
-           string.concat(i+" ");
+           string.append(i+" ");
         }
-        string.concat("\n");
+        string.append("\n");
         for(String i:this.licensor){
-            string.concat(i+" ");
+            string.append(i+" ");
         }
-        string.concat("\n");
+        string.append("\n");
         for(String i:this.studio){
-           string.concat(i+" ");
+           string.append(i+" ");
         }
-        string.concat("\n");
-       string.concat(
+        string.append("\n");
+       String str2=(
                 "Source: "+ this.source +"\n" +
                 "Score: " +this.scored + "\n" +
                 "Scored By: " +this.scoredby +"\n" +
                 "Total members: "+this.members +"\n"
         );
-       return string;
+       string.append(str2);
+       return string.toString();
 
 
 
